e1a23610e1e72e31a8c5eab66c946cd03a1e5ca2
Sławosz Sławiński <slawosz@cubiware.com>
2011-08-30 15:38:01 +0200

    created node knows its model

--- lib/grapher/node.rb
+++ lib/grapher/node.rb
@@ -1,27 +1,33 @@
 module Grapher
   class Node
+
     def self.model_to_node(model)
       node_class_name = model.to_s + "Node"
       eval <<-RUBY
         class ::#{node_class_name} < Neo4j::Rails::Model
-        end
-      RUBY
-      klass = node_class_name.constantize
-      klass.class_eval do
-        model.column_names.each do |column_name|
-          property column_name.to_sym
-        end
-      end
-        
-      copy_objects(model, klass)
+    class << self
+      attr_reader :base_model
     end
 
-    private
-    
-    def self.copy_objects(ar_class, node_class)
-      ar_class.all.each do |ar_object|
-        node_class.create!(ar_object.attributes)
-      end
+          @base_model = #{model}
+        end
+        RUBY
+        klass = node_class_name.constantize
+        klass.class_eval do
+          model.column_names.each do |column_name|
+            property column_name.to_sym
+          end
+        end
+
+        copy_objects(model, klass)
+  end
+
+  private
+
+  def self.copy_objects(ar_class, node_class)
+    ar_class.all.each do |ar_object|
+      node_class.create!(ar_object.attributes)
     end
   end
+end
 end
